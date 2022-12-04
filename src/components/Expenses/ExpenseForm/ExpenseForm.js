ead64479d6df607c00fc634e60e8aa3343fcf6f6
e186021 <venkateshkumar_shanmugasundar@amat.com>
2022-12-04 21:42:04 +0530

    different useState for different input

--- src/components/Expenses/ExpenseForm/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm/ExpenseForm.js
@@ -3,20 +3,35 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-    
+    //different usestate for different input variable
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+    const [date, setDate] = useState('')
+
+    const titleHandler = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const amountHandler = (event) => {
+        setAmount(event.target.value)
+    }
+
+    const dateHandler = (event) => {
+        setDate(event.target.value)
+    }
     return <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' ></input>
+                <input type='text' onChange={titleHandler} ></input>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' ></input>
+                <input type='number' min='0.01' step='0.01' onChange={amountHandler} ></input>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31'></input>
+                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateHandler}></input>
             </div>
         </div>
         <div className='new-expense__actions'>
