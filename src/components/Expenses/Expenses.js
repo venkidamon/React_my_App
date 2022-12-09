01d86286be4feae12c31ef82da376aa54a2261c2
e186021 <venkateshkumar_shanmugasundar@amat.com>
2022-12-09 21:52:45 +0530

    assignment 2 (Two way binding and child to parent)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card'
+import ExpenseFilter from "./ExpenseFilter/ExpenseFilter";
+import './Style/Expenses.css';
+import { useState } from "react";
 
 const Expenses = (props) => {
-    let result = props.data.map(ele => {
-        return (
-            <Card>
-                <ExpenseItem date = {ele.date} amount = {ele.amount} title = {ele.title}></ExpenseItem>
+    const [expenseFilter, setExpenseFilter] = useState('2019');
+
+    const expenseFilterSelected = (expenseFilterValue) => {
+        console.log('expenses')
+        setExpenseFilter(expenseFilterValue)
+        console.log(expenseFilter)
+    }
+    return (
+        <div>
+            {/* Two way binding */}
+            <ExpenseFilter onExpenseFilterSelected = {expenseFilterSelected} selected = {expenseFilter}></ExpenseFilter>
+            <Card className = 'expenses'>
+                <ExpenseItem title = {props.data[0].title}
+                amount = {props.data[0].amount}
+                date = {props.data[0].date}>
+                </ExpenseItem> 
+                <ExpenseItem title = {props.data[1].title}
+                amount = {props.data[1].amount}
+                date = {props.data[1].date}>
+                </ExpenseItem> 
+                <ExpenseItem title = {props.data[2].title}
+                amount = {props.data[2].amount}
+                date = {props.data[2].date}>
+                </ExpenseItem>
             </Card>
-        )
-    })
-    return result
+        </div>
+    )
 
 }
 
