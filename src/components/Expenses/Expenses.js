1aee52cf6e63fef18a39a278bc83eda21c82c403
e186021 <venkateshkumar_shanmugasundar@amat.com>
2022-12-17 21:15:17 +0530

    Better solution for Assignment 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,53 +6,28 @@ import { useState } from "react";
 
 const Expenses = (props) => {
     const [expenseFilter, setExpenseFilter] = useState('2019');
-    const [enableYearFilter, setEnableYearFilter] = useState(false);
-    const [newFilteredArr, setNewFilterArr] = useState([])
+
     const expenseFilterSelected = (expenseFilterValue) => {
         setExpenseFilter(expenseFilterValue)
-        let newFilteredArr1 = []
-        newFilteredArr1 = props.data.filter((ele) => {
-            if(ele.date.getFullYear().toString() === expenseFilterValue){
-                return ele
-            }
-        })
-        setNewFilterArr(newFilteredArr1)
-        setEnableYearFilter(true)
-        console.log(newFilteredArr)
     }
+
+    const filteredExpenses = props.data.filter(ele => {
+        return ele.date.getFullYear().toString() === expenseFilter
+    })
+    
     return (
         <div>
-            {enableYearFilter && newFilteredArr.length > 0 ? (
-                <div>
-                    <Card className='expenses'>
-                        <ExpenseFilter onExpenseFilterSelected={expenseFilterSelected} selected={expenseFilter}></ExpenseFilter>
-                        {newFilteredArr.map(expense => <ExpenseItem
-                            title={expense.title}
-                            amount={expense.amount}
-                            date={expense.date}
-                            // *key should be unique so that it will resolve the child conflict in the console
-                            // as react update the element one by one and replace all the element to avoid that we use key
-                            key={expense.id}></ExpenseItem>)}
-                    </Card>
-                </div>
-            ) : (
-                <div>
-                    {/* Two way binding */}
-                    <Card className='expenses'>
-                        <ExpenseFilter onExpenseFilterSelected={expenseFilterSelected} selected={expenseFilter}></ExpenseFilter>
-                        {props.data.map(expense => <ExpenseItem
-                            title={expense.title}
-                            amount={expense.amount}
-                            date={expense.date}
-                            // *key should be unique so that it will resolve the child conflict in the console
-                            // as react update the element one by one and replace all the element to avoid that we use key
-                            key={expense.id}></ExpenseItem>)}
-                    </Card>
-                </div>
-            )}
-
-
-
+            {/* Two way binding */}
+            <Card className='expenses'>
+                <ExpenseFilter onExpenseFilterSelected={expenseFilterSelected} selected={expenseFilter}></ExpenseFilter>
+                {filteredExpenses.map(expense => <ExpenseItem 
+                title = {expense.title} 
+                amount = {expense.amount} 
+                date = {expense.date}
+                // *key should be unique so that it will resolve the child conflict in the console
+                // as react update the element one by one and replace all the element to avoid that we use key
+                key = {expense.id}></ExpenseItem>)}
+            </Card>
         </div>
     )
 
